09175756875124adf3910dfc603bb2a0e69152cd
Yohanes Harke Wauran <harkewauran23@gmail.com>
2024-03-21 22:13:17 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+)
+
+type Weather struct {
+	Water int `json:"water"`
+	Wind  int `json:"wind"`
+}
+
+func (w *Weather) checkStatus() (resWater string, resWind string) {
+	switch {
+	case w.Water < 5:
+		resWater = "aman"
+	case w.Water >= 6 && w.Water <= 8:
+		resWater = "siaga"
+	default:
+		resWater = "bahaya"
+	}
+
+	switch {
+	case w.Wind < 6:
+		resWind = "aman"
+	case w.Wind >= 7 && w.Wind <= 15:
+		resWind = "siaga"
+	default:
+		resWind = "bahaya"
+	}
+	return resWater, resWind
+}
+
+
+func generateJSON(weather Weather) {
+	status := map[string]Weather{"status": weather}
+	
+	res, err := json.Marshal(status)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Printf("%s\n", res)
+
+	resWater, resWind := weather.checkStatus()
+	fmt.Printf("Status air: %s\n", resWater)
+	fmt.Printf("Status angin: %s\n", resWind)
+}
+
+func main() {
+	weather := Weather{}
+
+	for {
+		time.Sleep(2 * time.Second)
+
+		weather.Water = rand.Intn(15)
+		weather.Wind = rand.Intn(15)
+		generateJSON(weather)
+	}
+}
